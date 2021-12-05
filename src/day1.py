06f2c34582855cb13a1cb079c3aa686c078094fc
vveeps <54472340+vveeps@users.noreply.github.com>
2021-12-05 12:13:48 +0200

    add day1 code

---
+++ src/day1.py
@@ -0,0 +1,12 @@
+with open("inputs/day1.txt", "r") as f:
+    lines = f.read().splitlines()
+
+count = 0
+previous = float("inf")
+
+for line in lines:
+    if (depth := int(line)) > previous:
+        count += 1
+    previous = depth
+
+print(f"Measurements larger than previous: {count}")
